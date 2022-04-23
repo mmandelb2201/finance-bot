8e9a2c939235d7d65914b8c76e3e669e48032958
mmandelb2201 <mattrmandelbaum@gmail.com>
2022-04-23 18:21:36 -0400

    merge

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -6,14 +6,8 @@ const Home = () => {
   return <div>"Homie"</div>;
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 const Home = () => {
   return <div>"Homie"</div>;
 };
 
->>>>>>> parent of 47f6d7a (new suggestions)
-=======
->>>>>>> parent of 47f6d7a (new suggestions)
 export default Home;
