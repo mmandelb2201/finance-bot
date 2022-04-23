5b2d64028b0f66f900fa45ee9ad3c6f81349bb39
mmandelb2201 <mattrmandelbaum@gmail.com>
2022-04-23 18:21:08 -0400

    Revert "new suggestions"
    This reverts commit 47f6d7ae2431b58da702548df86afc6b5e94a03a.

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -7,10 +7,13 @@ const Home = () => {
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 const Home = () => {
   return <div>"Homie"</div>;
 };
 
+>>>>>>> parent of 47f6d7a (new suggestions)
+=======
 >>>>>>> parent of 47f6d7a (new suggestions)
 export default Home;
