14364842d40d0d1f0212a9b0def05c28146ed7c7
verdetea22 <mikayla.mount22@gmail.com>
2022-04-23 14:06:27 -0400

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -7,27 +7,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./components/pages/home";
 import SignUp from "./components/pages/signUp";
-import SignIn from "./components/pages/login";
 import InputData from "./components/pages/inputData";
 import Settings from "./components/pages/settings";
 import ViewAccount from "./components/pages/viewAccount";
+import { FirebaseAuthProvider } from "./contexts";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />{" "}
-          <Route path="/sign-up" element={<SignUp />} />{" "}
-          <Route path="/login" element={<SignIn />} />{" "}
-          <Route path="/input-data" element={<InputData />} />{" "}
-          <Route path="/settings" element={<Settings />} />{" "}
-          <Route path="/view-account" element={<ViewAccount />} />{" "}
-        </Routes>{" "}
-      </Router>{" "}
+      <FirebaseAuthProvider>
+        <Header />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/input-data" element={<InputData />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/view-account" element={<ViewAccount />} />
+          </Routes>
+        </Router>
+      </FirebaseAuthProvider>
     </div>
   );
 }
 
 export default App;
+
--- src/dist/donut.jsx
+++ src/dist/donut.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import ReactFC from "react-fusioncharts";
-//import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 
 import FusionCharts from "fusioncharts/core";
 import Doughnut2D from "fusioncharts/viz/doughnut2d";
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
 //Adding the chart as dependency to the core fusioncharts
-ReactFC.fcRoot(FusionCharts, Doughnut2D, FusionTheme);
 
 const chartConfigs = {
   type: "doughnut2d",
@@ -70,6 +69,6 @@ class Chart extends Component {
   }
 }
 
-//ReactDOM.render(<Chart />, document.getElementById("root"));
+ReactDOM.render(<Chart />, document.getElementById("root"));
 
 export default Chart;
