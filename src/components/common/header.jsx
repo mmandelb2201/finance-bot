86203d8bac918db1e634210e0dd5317b3f85d00f
Dylan James <djjames455@gmail.com>
2022-04-23 09:57:23 -0400

    Changed header: fully integrated react-bootstrap

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,42 +1,26 @@
 import React from "react";
 import "./common.css";
 
-import Dropdown from "react-bootstrap/Dropdown";
-import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
   return (
-    <section className="header">
-      <section className="header-top">
-        <section className="header-top__logo">
-          <a href="/" className="header-logo">
-            {" "}
-            <a className="header-logo-finance">Finance</a>
-            <a className="header-logo-bot">Bot</a>{" "}
-          </a>{" "}
-        </section>{" "}
-        <section className="header-top__welcome">
-          <a>Welcome,User</a>
-        </section>{" "}
-        <section>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" className="dropdown">
-              Account
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="/input-data">
-                Input Financial Data
-              </Dropdown.Item>
-              <Dropdown.Item href="/view-account">
-                See Account Details
-              </Dropdown.Item>
-              <Dropdown.Item href="/settings">Settings</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </section>{" "}
-      </section>{" "}
-    </section>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/homr">FinanceBot</Navbar.Brand>
+        
+      </Container>
+      <Container className="justify-content-end">
+      <Navbar.Text>Welcome, {"User"}</Navbar.Text>
+        <NavDropdown title="Action" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="/input-data">Input Financial Data</NavDropdown.Item>
+          <NavDropdown.Item href="/view-account">See Account Details</NavDropdown.Item>
+          <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
+        </NavDropdown>
+      </Container>
+    </Navbar>
   );
 }
 
