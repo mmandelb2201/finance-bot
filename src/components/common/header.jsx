4452dee85bc0165f627fbe66bae55877d9ceb88f
Dylan James <djjames455@gmail.com>
2022-04-23 13:56:11 -0400

    Minor change to header and get name of user

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useFirebaseAuth } from '../../hooks/useFirebaseAuth';
 import "./common.css";
 
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-function Header() {
+const Header = () => {
+  
+  const { name } = useFirebaseAuth();
+  const [username, setUsername] = useState(""); 
+
+  useEffect(() => {
+    setUsername(name);
+  }, [name]);
+  
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="/homr">FinanceBot</Navbar.Brand>
+        <Navbar.Brand href="/home">FinanceBot</Navbar.Brand>
         
       </Container>
       <Container className="justify-content-end">
-      <Navbar.Text>Welcome, {"User"}</Navbar.Text>
+      <Navbar.Text>{username ? `Welcome, ${username}`: "Sign In"}</Navbar.Text>
         <NavDropdown title="Action" id="navbarScrollingDropdown">
           <NavDropdown.Item href="/input-data">Input Financial Data</NavDropdown.Item>
           <NavDropdown.Item href="/view-account">See Account Details</NavDropdown.Item>
