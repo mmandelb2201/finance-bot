75adb427fbe40791a2870cb1b8129394d341216e
mmandelb2201 <mattrmandelbaum@gmail.com>
2022-04-23 17:55:03 -0400

    merging

--- src/App.js
+++ src/App.js
@@ -6,23 +6,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./components/pages/home";
-<<<<<<< HEAD
-import SignUp from "./components/pages/Auth/SignUp";
-import InputData from "./components/pages/inputData";
-import Settings from "./components/pages/settings";
-import ViewAccount from "./components/pages/viewAccount";
-<<<<<<< HEAD
-import { FirebaseAuthProvider } from './contexts';
-=======
 import SignUp from "./components/pages/signUp";
 import SignIn from "./components/pages/login";
 import InputData from "./components/pages/inputData";
 import Settings from "./components/pages/settings";
 import ViewAccount from "./components/pages/viewAccount";
->>>>>>> parent of 1436484 (fixing merge conflicts)
-=======
-import { FirebaseAuthProvider } from "./contexts";
->>>>>>> 269604165286a3cae056c015b3e59e3f65e36123
 
 function App() {
   return (
--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import { useFirebaseAuth } from "../../hooks/useFirebaseAuth";
-import "./common.css";
-
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
-const Header = () => {
-  const { name } = useFirebaseAuth();
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    setUsername(name);
-  }, [name]);
-
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="/home">FinanceBot</Navbar.Brand>
-      </Container>
-      <Container className="justify-content-end">
-        <Navbar.Text>
-          {username ? `Welcome, ${username}` : "Sign In"}
-        </Navbar.Text>
-        <NavDropdown title="Action" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="/input-data">
-            Input Financial Data
-          </NavDropdown.Item>
-          <NavDropdown.Item href="/view-account">
-            See Account Details
-          </NavDropdown.Item>
-          <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
-        </NavDropdown>
-      </Container>
-    </Navbar>
-=======
 import React from "react";
 import "./common.css";
 
@@ -76,8 +38,7 @@ function Header() {
         </section>{" "}
       </section>{" "}
     </section>
->>>>>>> parent of 1436484 (fixing merge conflicts)
   );
-};
+}
 
 export default Header;
