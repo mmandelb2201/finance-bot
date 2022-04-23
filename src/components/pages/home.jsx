8f318eddfa39d6187db94607a45fdfb928ad66c6
mmandelb2201 <mattrmandelbaum@gmail.com>
2022-04-23 18:19:09 -0400

    merge

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import Chart from "../../dist/donut";
 import "./pages.css";
 
+<<<<<<< HEAD
 function Home() {
   
   return <div></div>
   
 }
+=======
+const Home = () => {
+  return <div>"Homie"</div>;
+};
+>>>>>>> parent of 47f6d7a (new suggestions)
 
 export default Home;
